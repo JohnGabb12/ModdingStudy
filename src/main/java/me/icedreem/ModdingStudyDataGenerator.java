57ac9939781f959d554a338981a6e4beeccc68db
JohnGabb12 <valencerina.johngabrielle@ue.edu.ph>
2025-12-25 21:11:47 +0800

    init

---
+++ src/main/java/me/icedreem/ModdingStudyDataGenerator.java
@@ -0,0 +1,11 @@
+package me.icedreem;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModdingStudyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
